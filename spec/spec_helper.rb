e19dc29af069580ada2e473d47f74c00938a56b5
Marco Rojo <mrmarcondes@gmail.com>
2013-01-17 17:46:18 -0200

    reestruturando diretorios

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
-Dir['./lib/domain/*.rb'].each {|file| require file }
-Dir['./lib/service/*.rb'].each {|file| require file }
+Dir['./lib/geo-core/domain/*.rb'].each {|file| require file }
+Dir['./lib/geo-core/service/*.rb'].each {|file| require file }
 
 require 'rspec'
 require 'rspec/autorun'
